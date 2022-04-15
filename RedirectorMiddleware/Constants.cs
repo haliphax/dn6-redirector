3c52633a939158409a29e7a8cdc97b06d485c2fc
haliphax <haliphax@gmail.com>
2022-04-15 16:53:28 -0500

    add <value> comment to constants for now

--- RedirectorMiddleware/Constants.cs
+++ RedirectorMiddleware/Constants.cs
@@ -8,9 +8,15 @@ public static class Constants
 	/// <summary>
 	/// The configuration section containing the middleware/API configuration
 	/// </summary>
+	/// <value>
+	/// Todd.Redirector
+	/// </value>
   public const string ConfigSection = "Todd.Redirector";
 	/// <summary>
 	/// Default API refresh delay (10 seconds)
 	/// </summary>
+	/// <value>
+	/// 10000
+	/// </value>
   public const uint DefaultApiRefreshDelay = 10 * 1000;
 }
